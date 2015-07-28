e08f8c5440c101b11d9440e5b10dc861f409ea8d
Chris Roupp <grumbly09@hotmail.com>
2015-07-27 17:22:58 -0700

    More changes - Adding big desk.

--- elasticsearch-aws-custom/recipes/esplugins.rb
+++ elasticsearch-aws-custom/recipes/esplugins.rb
@@ -8,4 +8,14 @@ script "install_plugin_es_head" do
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
 
+script "install_plugin_es_bigdesk" do
+    interpreter "bash"
+    user "root"
+    cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+    code <<-EOH
+    plugin -install lukas-vlcek/bigdesk
+    EOH
+    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/bigdesk") }
+end
+
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
